8882c0a7d78bd71ad3299cbd9e233392d68419a5
Ryan Godfrey <regodfrey@pm.me>
2019-12-03 19:13:37 -0800

    Switched to using m3 for xor, built psum cell

---
+++ psum.cmd
@@ -0,0 +1,67 @@
+w x y p cin z cout
+vector in x y p cin
+vector out z cout
+
+setvector in 0000
+s
+assert out 00
+
+setvector in 0001
+s
+assert out 10
+
+setvector in 0010
+s
+assert out 10
+
+setvector in 0011
+s
+assert out 01
+
+setvector in 0100
+s
+assert out 00
+
+setvector in 0101
+s
+assert out 10
+
+setvector in 0110
+s
+assert out 10
+
+setvector in 0111
+s
+assert out 01
+
+setvector in 1000
+s
+assert out 00
+
+setvector in 1001
+s
+assert out 10
+
+setvector in 1010
+s
+assert out 10
+
+setvector in 1011
+s
+assert out 01
+
+setvector in 1100
+s
+assert out 10
+
+setvector in 1101
+s
+assert out 01
+
+setvector in 1110
+s
+assert out 01
+
+setvector in 1111
+s
+assert out 11
